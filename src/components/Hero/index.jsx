f7a939c6dd8e423ca024a8e56495d9d6346c6aed
Avas <avas.bajracharya@renegadeinsurance.com>
2023-12-19 17:05:24 +0545

    feat: added styles for home page

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,16 +3,18 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="min-h-screen flex">
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center relative xl:w-2/5">
         <p className="text-lg font-montserrat text-coral-red">
           Our Summer collections
         </p>
-        <h1>
-          The New Arrival
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span>Nike</span> Shoes
+          <span className="text-coral-red mt-3 inline-block">Nike</span> Shoes
         </h1>
-        <p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
