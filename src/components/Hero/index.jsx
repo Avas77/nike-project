aa33ea8b1839d4c458ba38a5499699f6e58bc83b
Avas <avas.bajracharya@renegadeinsurance.com>
2023-12-19 16:33:58 +0545

    feat: style navbar

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="min-h-screen flex">
+      <div className="flex flex-col justify-center">
+        <p className="text-lg font-montserrat text-coral-red">
+          Our Summer collections
+        </p>
+        <h1>
+          The New Arrival
+          <br />
+          <span>Nike</span> Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+      </div>
+      <div className="flex-1">World</div>
+    </div>
+  );
 };
 
 export default Hero;
