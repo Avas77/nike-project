e86f85e0863fb98528c2ad357e93f54116160683
Avas <avas.bajracharya@renegadeinsurance.com>
2023-12-24 13:08:47 +0545

    feat: added services components

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { services } from "../../constants/services";
+import ServiceCard from "../ServiceCard";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section
+      id="services"
+      className="max-container flex justify-center flex-wrap gap-9"
+    >
+      {services.map((service) => (
+        <ServiceCard
+          key={service.id}
+          name={service.name}
+          description={service.description}
+          icon={service.icon}
+        />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
