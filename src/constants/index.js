4ade8e5aee1c344b31b15a217539a7614209ed5e
Avas <avas.bajracharya@renegadeinsurance.com>
2023-12-24 09:50:41 +0545

    feat: added popular products componenets

---
+++ src/constants/index.js
@@ -0,0 +1,32 @@
+import { shoe5, shoe4, shoe6, shoe7 } from "../assets/images/index";
+
+export const shoes = [
+  {
+    id: 1,
+    name: "Nike Air Jordan-01",
+    rating: "(4.5)",
+    price: "$200.20",
+    path: shoe4,
+  },
+  {
+    id: 2,
+    name: "Nike Air Jordan-02",
+    rating: "(4.2)",
+    price: "$201.20",
+    path: shoe5,
+  },
+  {
+    id: 1,
+    name: "Nike Air Jordan-001",
+    rating: "(4.0)",
+    price: "$220.20",
+    path: shoe6,
+  },
+  {
+    id: 1,
+    name: "Nike Air Jordan-10",
+    rating: "(4.8)",
+    price: "$270.20",
+    path: shoe7,
+  },
+];
