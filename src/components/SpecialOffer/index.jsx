d55a2f7f4113139c29b1c81e25cb44455215c82f
Avas <avas.bajracharya@renegadeinsurance.com>
2023-12-25 20:58:53 +0545

    feat: added components and styles for offers section

--- src/components/SpecialOffer/index.jsx
+++ src/components/SpecialOffer/index.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { offer } from "../../assets/images";
+import CTAButton from "../CTAButton";
 
 const SpecialOffer = () => {
-  return <div>SpecialOffer</div>;
+  return (
+    <section
+      id="special-offers"
+      className="flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container"
+    >
+      <div>
+        <img src={offer} alt="offers" />
+      </div>
+      <div>
+        <h2 className="font-palanquin capitalize text-4xl lg:max-w-lg font-bold">
+          <span className="text-coral-red">Special</span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <CTAButton label="Shop now" showArrow />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffer;
