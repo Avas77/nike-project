3c96b9dba237463b5c5358c1eae6ce1d114151bc
Avas <avas.bajracharya@renegadeinsurance.com>
2023-12-27 20:49:24 +0545

    feat: added router with pages home and products

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-import {
-  CustomerReviews,
-  Nav,
-  Hero,
-  Footer,
-  PopularProducts,
-  Services,
-  SpecialOffer,
-  Subscribe,
-  SuperQuality,
-} from "./components";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+]);
 
 const App = () => {
-  return (
-    <main className="relative">
-      <Nav />
-      <section className="xl:padding-l wide:padding-r padding-b">
-        <Hero />
-      </section>
-      <section className="padding">
-        <PopularProducts />
-      </section>
-      <section className="padding">
-        <SuperQuality />
-      </section>
-      <section className="padding-x py-10">
-        <Services />
-      </section>
-      <section className="padding">
-        <SpecialOffer />
-      </section>
-      <section className="bg-pale-blue padding">
-        <CustomerReviews />
-      </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
-        <Subscribe />
-      </section>
-      <section className=" bg-black padding-x padding-t pb-8">
-        <Footer />
-      </section>
-    </main>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
