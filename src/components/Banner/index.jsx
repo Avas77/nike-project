0542dcf73f58afbde44c87f1d3de9e6c17d3595d
Avas <avas.bajracharya@renegadeinsurance.com>
2023-12-30 19:51:51 +0545

    feat: added filters tab components and styles

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="bg-banner-grey min-h-[60px] flex justify-center items-center">
+      <p>MEMBERS: FREE SHIPPING ON ORDERS $50+</p>
+    </div>
+  );
+};
+
+export default Banner;
