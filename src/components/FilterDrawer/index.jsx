fb200c3e2f75e32fd19599998e5a5bad857a75e8
Avas <avas.bajracharya@renegadeinsurance.com>
2023-12-30 20:31:33 +0545

    feat: added products components and styles

--- src/components/FilterDrawer/index.jsx
+++ src/components/FilterDrawer/index.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const FilterDrawer = () => {
   return (
-    <div className="pb-5 mb-5 border-b border-solid border-b-border-gray">
-      <h6 className="text-[16px]">Pick Up Today</h6>
+    <div>
+      <div className="pb-5 mb-5 border-b border-solid border-b-border-gray">
+        <h6 className="text-[16px] font-medium">Pick Up Today</h6>
+      </div>
+      <ul>
+        <li className="pb-[10px] pr-4 text-[16px] font-medium">Jordan</li>
+        <li className="pb-[10px] pr-4 text-[16px] font-medium">Lifestyle</li>
+        <li className="pb-[10px] pr-4 text-[16px] font-medium">Running</li>
+        <li className="pb-[10px] pr-4 text-[16px] font-medium">Basketball</li>
+        <li className="pb-[10px] pr-4 text-[16px] font-medium">
+          Training & Gym
+        </li>
+      </ul>
     </div>
   );
 };
