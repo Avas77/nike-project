9a9ee946cec6db8a1bce102f05714b45ccce4362
Avas <avas.bajracharya@renegadeinsurance.com>
2024-01-09 12:00:44 +0545

    feat: added sign in component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Product from "./pages/Product";
+import SignIn from "./pages/Signin";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
   {
     path: "/products/:id",
     element: <Product />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
   },
 ]);
 
