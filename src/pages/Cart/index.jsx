fd26a6088e9341472b8ab47d24cf1b26aaac47e4
Avas <avas.bajracharya@renegadeinsurance.com>
2024-01-12 17:07:10 +0545

    feat: added dynamic data to cart page

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Footer, Nav } from "../../components";
 import { getToken } from "../../utils";
 import { viewCart } from "../../api";
@@ -7,6 +7,7 @@ const Cart = () => {
   const [order, setOrder] = useState([]);
   const userSession = getToken();
   const email = userSession?.user?.email;
+  const price = order.length > 0 ? order[0].price : 0;
 
   useEffect(() => {
     viewCart({
@@ -43,12 +44,19 @@ const Cart = () => {
                 </div>
               </div>
             ))}
-          <div>
+          <div className="mb-6">
             <p className="text-[16px] font-medium">Shipping</p>
             <div className="text-[16px]">
               Arrives by Fri, Jan 19{" "}
               <span className="font-small">Edit Location</span>
             </div>
+            <p className="text-[16px] font-medium mt-[24px]">Free Pickup</p>
+            <span className="font-small">Edit Location</span>
+          </div>
+          <hr className="pb-6" />
+          <div>
+            <h1 className="font-medium text-[24px]">Favourites</h1>
+            <p>Want to view your favourites?</p>
           </div>
         </div>
         <div className="basis-96">
@@ -57,6 +65,7 @@ const Cart = () => {
             <span className="mb-2">Do you have a promo code?</span>
             <div className="mb-2 flex justify-between">
               <span>Subtotal</span>
+              <span>{price}</span>
             </div>
             <div className="mb-2 flex justify-between">
               <span>Estimated Shipping & Handling</span>
@@ -69,8 +78,15 @@ const Cart = () => {
             <hr />
             <div className="my-3 py-4 flex justify-between">
               <span>Total</span>
-              <span>$167</span>
+              <span>{price}</span>
             </div>
+            <hr />
+            <button className="w-full bg-black text-white px-6 py-5 rounded-full mt-12">
+              Checkout
+            </button>
+            <button className="w-full mb-6 bg-white text-black px-6 py-5 rounded-full mt-3 border border-black">
+              PayPal
+            </button>
           </div>
         </div>
       </div>
